3ad2d28a950869781374e75e2ecdb6fffe6e5b78
Devin Ekadeni <devinekadeni@gmail.com>
2024-08-18 07:59:13 +0700

    feat: improve validation for giving vote rights

--- helpers/constant.ts
+++ helpers/constant.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 export const userPrivateKey = process.env.PRIVATE_KEY || "";
+export const ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/
